e53e05915902c3c4b740302264239d3c1f388f2d
Filip Sufitchi <filips@imagemattersllc.com>
2017-10-19 11:52:54 -0400

    Make scripts executable

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $(dirname $0))  # Return to root
 
-go build -v -o pubg-hack
+GOOS=linux GOARCH=amd64 go build -v -o pubg-hack
 npm run webpack
 
 tar cvvfz pubg-hack.tar.gz pubg-hack dist
