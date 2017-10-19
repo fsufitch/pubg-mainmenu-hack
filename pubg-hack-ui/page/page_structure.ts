7def84504bd79fe9a26aa17331cef8ccfb45d3b4
Filip Sufitchi <filips@imagemattersllc.com>
2017-10-19 11:46:31 -0400

    Add console to hacked page

--- pubg-hack-ui/page/page_structure.ts
+++ pubg-hack-ui/page/page_structure.ts
@@ -24,4 +24,31 @@ export function renderPostHack(apiHost: string, cbFinished: () => void) {
   console.log(apiHost);
   modalDiv.find('#apiUrl').html(apiHost);
   modalDiv.find('#continue').click(() => cbFinished());
+
+  modalDiv.find('#devToggleLink').click(() => {
+    modalDiv.find('#devToggle').hide();
+    modalDiv.find('#console').show();
+  });
+
+  runner($('#consoleInput'), $('#console ul.console-output'));
+}
+
+function runner(source: JQuery<HTMLElement>, destination: JQuery<HTMLElement>) {
+  let appendOutput = (prefix: string, output: string) =>
+    $('<li>', {text: `${prefix} ${output}`}).appendTo(destination);
+
+  let run = (cmd: string) => {
+    appendOutput('>', cmd);
+    source.val('');
+    try {
+      appendOutput('<-', eval(cmd));
+    } catch (err) {
+      appendOutput('<!', err);
+    }
+    destination.scrollTop(destination.prop('scrollHeight'));
+  }
+
+  source.keypress(e => {
+    if (e.which == 13) run(<string>source.val());
+  })
 }
